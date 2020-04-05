27a66e383f58c4c5ae83305e6232e792683b1249
Mike Borozdin <mike.borozdin@gmail.com>
2020-04-05 15:00:21 +0100

    added US data

--- src/app/DataStore.ts
+++ src/app/DataStore.ts
@@ -3,16 +3,62 @@ import Papa from 'papaparse';
 import { Timeseries } from './Timeseries';
 
 export default class DataStore {
-  private COUNTRY_DATA_URL =
+  private NON_US_DATA_URL =
     'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv';
+
+  private US_DATA_URL =
+    'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv';
 
   @observable
   public data: Timeseries;
 
+  private async getUsData(): Promise<number[]> {
+    return new Promise((resolve) => {
+      Papa.parse(this.US_DATA_URL, {
+        download: true,
+        header: true,
+        complete: (results: Papa.ParseResult) => {
+          const rawData = results.data;
+
+          const datesStartsFromIndex = 11;
+
+          let usValues: number[] = [];
+
+          for (let i = 0; i < rawData.length - 1; i++) {
+            const rawStateData = rawData[i];
+
+            if (i === 0) {
+              usValues = Object.keys(rawStateData)
+                .slice(datesStartsFromIndex)
+                .map((date: string) => parseInt(rawStateData[date]));
+            } else {
+              usValues = Object.keys(rawStateData)
+                .slice(datesStartsFromIndex)
+                .map(
+                  (date: string, index: number) =>
+                    parseInt(rawStateData[date]) + usValues[index]
+                );
+            }
+          }
+
+          resolve(usValues);
+        },
+      });
+    });
+  }
+
   @action.bound
   public async getData() {
-    const promise: Promise<void> = new Promise<void>((resolve) => {
-      Papa.parse(this.COUNTRY_DATA_URL, {
+    const timeseries = await this.fetchAndProcessNonUsData();
+
+    timeseries.countries['United States'] = await this.getUsData();
+
+    this.data = timeseries;
+  }
+
+  private async fetchAndProcessNonUsData(): Promise<Timeseries> {
+    return new Promise<Timeseries>((resolve) => {
+      Papa.parse(this.NON_US_DATA_URL, {
         download: true,
         header: true,
         complete: (results: Papa.ParseResult) => {
@@ -48,13 +94,9 @@ export default class DataStore {
             }
           }
 
-          this.data = timeseries;
-
-          resolve();
+          resolve(timeseries);
         },
       });
     });
-
-    return promise;
   }
 }
