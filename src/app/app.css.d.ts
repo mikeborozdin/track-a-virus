200e2a7a7b2d34d5d08e6fdff17c17c2ee1b5686
Mike Borozdin <mike.borozdin@gmail.com>
2020-04-08 08:31:28 +0100

    responsive menu

--- src/app/app.css.d.ts
+++ src/app/app.css.d.ts
@@ -1,9 +1,17 @@
 export interface IAppCss {
+  banner: string;
   'col1-320px': string;
   'col2-768px': string;
   'col3-1024px': string;
+  content: string;
+  header: string;
+  hidden: string;
   logo: string;
+  menu: string;
+  menuButton: string;
+  menuItem: string;
   'span-all-col': string;
+  visible: string;
 }
 
 export const locals: IAppCss;
--- src/app/app.tsx
+++ src/app/app.tsx
@@ -21,6 +21,8 @@ interface SelectOption {
 }
 
 const App: React.FC<Props> = ({ rootStore }) => {
+  const [toggleMenu, setToggleMenu] = useState<boolean>(false);
+
   const [countriesToCompare, setCountriesToCompare] = useState<SelectOption[]>([
     {
       value: 'World',
@@ -39,136 +41,131 @@ const App: React.FC<Props> = ({ rootStore }) => {
   }, [countriesToCompare, rootStore.allCases, rootStore.allDeaths]);
 
   return (
-    <div
-      className={`
+    <>
+      <div className={styles.header}>
+        <div className={styles.banner}>
+          <div className={styles.logo}>
+            <a href='/'>
+              <img src={icon} />
+              Track a Virus
+            </a>
+          </div>
+          <div>
+            <a href='/'>Covid-19 Dashboard</a>
+          </div>
+        </div>
+
+        <a
+          href='#'
+          onClick={() => setToggleMenu(!toggleMenu)}
+          className={styles.menuButton}
+        >
+          &#9776;
+        </a>
+
+        <ul className={`${styles.menu} ${!toggleMenu ? styles.hidden : ''}`}>
+          <li>
+            <a href='/' className={styles.menuItem}>
+              Home
+            </a>
+          </li>
+          <li>
+            <a href='/all-posts/' className={styles.menuItem}>
+              All posts
+            </a>
+          </li>
+          <li>
+            <a href='/talks/' className={styles.menuItem}>
+              Talks
+            </a>
+          </li>
+          <li>
+            <a href='/about/' className={styles.menuItem}>
+              About me
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div
+        className={`
+        ${styles.content}
         ${styles['col1-320px']}
         ${styles['col2-768px']}
         ${styles['col3-1024px']}
       `}
-    >
-      {rootStore.allCases && rootStore.allDeaths && (
-        <>
-          <div className={`${styles['span-all-col']} ${styles['logo']}`}>
-            <h1>
-              <img src={icon} />
-              Track a Virus: Covid-19 Dashboard
-            </h1>
-            <p>(data updated on {rootStore.dateUpdated.toDateString()})</p>
-          </div>
+      >
+        {rootStore.allCases && rootStore.allDeaths && (
+          <>
+            <div className={styles['span-all-col']}>
+              <label htmlFor='countrySelector'>
+                Select a country or a few to dive in & compare
+              </label>
+              <Select
+                inputId='countrySelector'
+                options={getCountrySelectOptions(rootStore.countries)}
+                isMulti
+                value={countriesToCompare}
+                onChange={(selected) => {
+                  setCountriesToCompare(selected as SelectOption[]);
+                }}
+                placeholder="Select countries to compare. Type 'World' for the worldwide data"
+              />
+            </div>
+            {rootStore.selectedCountriesCases && (
+              <>
+                <div className={styles['span-all-col']}>
+                  <h1>Confirmed cases</h1>
+                </div>
+                <div>
+                  <DailyCases
+                    data={rootStore.selectedCountriesCases}
+                    countryColors={rootStore.countryColors}
+                  />
+                </div>
+                <div>
+                  <DailyIncrease
+                    data={rootStore.selectedCountriesCases}
+                    countryColors={rootStore.countryColors}
+                  />
+                </div>
+                <div>
+                  <DailyIncreasePercentage
+                    data={rootStore.selectedCountriesCases}
+                    countryColors={rootStore.countryColors}
+                  />
+                </div>
+              </>
+            )}
 
-          {/* <div className={styles['span-all-col']}>
-            <h1>Worldwide - confirmed cases</h1>
-          </div>
-          <div>
-            <DailyCases
-              data={rootStore.aggregatedGlobalCases}
-              countryColors={{ global: 'red' }}
-            />
-          </div>
-          <div>
-            <DailyIncrease
-              data={rootStore.aggregatedGlobalCases}
-              countryColors={{ global: 'red' }}
-            />
-          </div>
-          <div>
-            <DailyIncreasePercentage
-              data={rootStore.aggregatedGlobalCases}
-              countryColors={{ global: 'red' }}
-            />
-          </div>
-
-          <div className={styles['span-all-col']}>
-            <h1>Worldwide - confirmed deaths</h1>
-          </div>
-          <div>
-            <DailyCases
-              data={rootStore.aggregatedGlobalDeaths}
-              countryColors={{ global: 'red' }}
-            />
-          </div>
-          <div>
-            <DailyIncrease
-              data={rootStore.aggregatedGlobalDeaths}
-              countryColors={{ global: 'red' }}
-            />
-          </div>
-          <div>
-            <DailyIncreasePercentage
-              data={rootStore.aggregatedGlobalDeaths}
-              countryColors={{ global: 'red' }}
-            />
-          </div> */}
-
-          <div className={styles['span-all-col']}>
-            <label htmlFor='countrySelector'>
-              Select a country or a few to dive in & compare
-            </label>
-            <Select
-              inputId='countrySelector'
-              options={getCountrySelectOptions(rootStore.countries)}
-              isMulti
-              value={countriesToCompare}
-              onChange={(selected) => {
-                setCountriesToCompare(selected as SelectOption[]);
-              }}
-              placeholder="Select countries to compare. Type 'World' for the worldwide data"
-            />
-          </div>
-          {rootStore.selectedCountriesCases && (
-            <>
-              <div className={styles['span-all-col']}>
-                <h1>Confirmed cases</h1>
-              </div>
-              <div>
-                <DailyCases
-                  data={rootStore.selectedCountriesCases}
-                  countryColors={rootStore.countryColors}
-                />
-              </div>
-              <div>
-                <DailyIncrease
-                  data={rootStore.selectedCountriesCases}
-                  countryColors={rootStore.countryColors}
-                />
-              </div>
-              <div>
-                <DailyIncreasePercentage
-                  data={rootStore.selectedCountriesCases}
-                  countryColors={rootStore.countryColors}
-                />
-              </div>
-            </>
-          )}
-
-          {rootStore.selectedCountriesDeaths && (
-            <>
-              <div className={styles['span-all-col']}>
-                <h1>Confirmed deaths</h1>
-              </div>
-              <div>
-                <DailyCases
-                  data={rootStore.selectedCountriesDeaths}
-                  countryColors={rootStore.countryColors}
-                />
-              </div>
-              <div>
-                <DailyIncrease
-                  data={rootStore.selectedCountriesDeaths}
-                  countryColors={rootStore.countryColors}
-                />
-              </div>
-              <div>
-                <DailyIncreasePercentage
-                  data={rootStore.selectedCountriesDeaths}
-                  countryColors={rootStore.countryColors}
-                />
-              </div>
-            </>
-          )}
-        </>
-      )}
-    </div>
+            {rootStore.selectedCountriesDeaths && (
+              <>
+                <div className={styles['span-all-col']}>
+                  <h1>Confirmed deaths</h1>
+                </div>
+                <div>
+                  <DailyCases
+                    data={rootStore.selectedCountriesDeaths}
+                    countryColors={rootStore.countryColors}
+                  />
+                </div>
+                <div>
+                  <DailyIncrease
+                    data={rootStore.selectedCountriesDeaths}
+                    countryColors={rootStore.countryColors}
+                  />
+                </div>
+                <div>
+                  <DailyIncreasePercentage
+                    data={rootStore.selectedCountriesDeaths}
+                    countryColors={rootStore.countryColors}
+                  />
+                </div>
+              </>
+            )}
+          </>
+        )}
+      </div>
+    </>
   );
 };
 
